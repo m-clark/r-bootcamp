99903496af285f5cd34d22f22cddb06ef93c1cfe
Michael Clark <michael.clark@nd.edu>
2015-07-17 15:45:34 -0400

    initial commit

---
+++ gettingStarted.R
@@ -0,0 +1,11 @@
+# - Getting started
+#   - Installing R
+#   - Installing Rstudio
+#     - Getting to know the power of Rstudio
+#     - Installing additional functionality
+
+
+install.packages('ggplot2')
+library(ggplot2)
+
+# install the ggvis, car and psych packages; load one of them
